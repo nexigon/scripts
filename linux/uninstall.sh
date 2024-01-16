10ef7d76f1d7d31af2e19fa9e5d0c9d27d8971d1
nexigon <139718004+nexigon@users.noreply.github.com>
2024-01-16 00:04:44 +0000

    script inicio

---
+++ linux/uninstall.sh
@@ -0,0 +1,22 @@
+#MENU DE REMOCAO DE APLICATIVOS NO LINUX
+
+#!/bin/bash
+x="apps"
+menu()
+{
+    while true &x !="apps"
+    do
+    clear
+    #OPCOES DO MENU
+    echo "==========================================="
+    echo "linux script ubuntu/derivados"
+    echo "feito por nexigonprime"
+    echo ""
+    echo ""
+    echo "1) remover apps"
+    echo "2) instalar apps(por sudo apt install)"
+    echo "3) atualizar sistema"
+    echo "4)reparar erros"
+    echo "5) reconfigurar"
+    echo "==========================================="
+}
